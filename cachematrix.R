29a7f5369843fe69edd3a5aba6d4dacdb4eba23d
diantar <diantar@gmail.com>
2019-01-31 21:49:57 +0000

    this was crazy

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
-######
-## Write a short comment describing this function test rtr
-
+## This is a second assignment where you become stronger
+## Function supposed to demonstrate a lexical scoping on a purely theoretical example, and without proper explanation
+    
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
+    
 }
